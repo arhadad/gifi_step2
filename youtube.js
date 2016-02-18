505fd7b0ab4f789af707d0d1840a76ddadb54d40
Ariel <erik.hadad81@gmail.com>
2016-02-18 22:55:56 +0200

    Added files via upload

---
+++ youtube.js
@@ -0,0 +1,27 @@
+﻿
+var player;
+          
+function onYouTubePlayerAPIReady(){
+  player = new YT.Player('media_player',{ events: { 'onReady': onPlayerReady}});}
+  
+  function onPlayerReady(event) {
+  // bind events
+  var playButton = document.getElementById("play-button");
+  playButton.addEventListener("click", function() {
+    player.playVideo();
+  });
+  
+  var pauseButton = document.getElementById("pause-button");
+  pauseButton.addEventListener("click", function() {
+    player.pauseVideo();
+  });
+}
+
+/*
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag,firstScriptTag);
+
+*/
+
